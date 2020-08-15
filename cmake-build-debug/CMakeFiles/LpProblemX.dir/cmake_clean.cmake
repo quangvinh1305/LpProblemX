682211b4f9748e7a4120d2ee95931bb1641b9d66
Vinh Nguyen <quangvinh.1305@gmail.com>
2020-08-15 07:22:04 +0700

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/LpProblemX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LpProblemX.dir/main.cpp.o"
+  "LpProblemX"
+  "LpProblemX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LpProblemX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
